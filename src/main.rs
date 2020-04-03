3b95852c4b8b85dd565922e70dc87d83b2c017ca
Max Cohn <maxc4321@gmail.com>
2020-04-03 19:06:30 -0400

    non-listening finished

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,39 @@
-use std::net::Ipv4Addr;
 use std::net::TcpStream;
 use std::env;
 use std::io::prelude::*;
+use std::io::{BufRead, BufReader};
+use std::thread;
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
     let ip = &args[1];
     let port = &args[2];
-    let mut stream = TcpStream::connect(format!("{}:{}", ip, port))?;
+    let mut writer = TcpStream::connect(format!("{}:{}", ip, port))?;
+    let mut reader = BufReader::new(writer.try_clone()?);
 
-    stream.write("nnldnsdknlsd".as_bytes())?;
+    thread::spawn(move || {
+        loop {
+            let mut buf = String::new();
+            reader.read_line(&mut buf).unwrap();
+            print!("{}", buf);
+        }
+    });
+
+    for line in std::io::stdin().lock().lines() {
+        match line {
+            Ok(line) => {
+                writer.write(line.as_bytes())?;
+                writer.write(&[b'\n'])?;
+            },
+            Err(_) => panic!("asd"),
+        };
+    }
+    //stream.write("nnldnsdknlsd".as_bytes())?;
 
 
     Ok(())
+}
+
+fn listen_thread() {
+
 }
