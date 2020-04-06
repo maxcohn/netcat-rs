d89de0f27ea21dd88b209cd5ba44afc78d7385d6
Max Cohn <maxc4321@gmail.com>
2020-04-05 22:16:15 -0400

    Added listen option

---
+++ README.md
@@ -0,0 +1,15 @@
+# netcat-rs
+
+`netcat` implemented in Rust.
+
+I love `netcat` (often aliased as `nc`). I wanted to write it in Rust. Here we are.
+
+I might go ahead and write some Rust implementations of other common Unix utilities
+in the future.
+
+Currently we only support an outgoing and incoming (`-l`) connection.
+
+## TODO
+
+* Add IPv6 support
+* Better error handling
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,41 @@
-use std::net::TcpStream;
-use std::env;
+use std::net::{TcpStream, TcpListener};
 use std::io::prelude::*;
 use std::io::{BufRead, BufReader};
 use std::thread;
+use clap::{App, Arg, AppSettings};
 
 fn main() -> std::io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    let ip = &args[1];
-    let port = &args[2];
-    let mut writer = TcpStream::connect(format!("{}:{}", ip, port))?;
+    // get command line arguments
+    let matches = App::new("netcat")
+        .setting(AppSettings::AllowMissingPositional)
+        .arg(Arg::with_name("listen")
+            .short("l")
+            .long("listen"))
+        .arg(Arg::with_name("ip"))
+        .arg(Arg::with_name("port").required(true))
+        .get_matches();
+    
+    // get user defined IP if exists, else use the default of 0.0.0.0
+    let ip = if matches.is_present("ip") {
+        matches.value_of("ip").unwrap()
+    } else {
+        "0.0.0.0"
+    };
+
+    let port = matches.value_of("port").unwrap();
+
+    let mut writer;
+    if matches.is_present("listen") {
+        let listener = TcpListener::bind(format!("0.0.0.0:{}", port)).unwrap();
+        let (conn, _) = listener.accept().unwrap();
+        writer = conn;
+    } else {
+        writer = TcpStream::connect(format!("{}:{}", ip, port))?;
+    }
+
     let mut reader = BufReader::new(writer.try_clone()?);
 
+    // create the thread to read from the other end of the connection
     thread::spawn(move || {
         loop {
             let mut buf = String::new();
@@ -19,21 +44,16 @@ fn main() -> std::io::Result<()> {
         }
     });
 
+    // read from stdin (line by line) and write it to the TCP stream
     for line in std::io::stdin().lock().lines() {
         match line {
             Ok(line) => {
                 writer.write(line.as_bytes())?;
                 writer.write(&[b'\n'])?;
             },
-            Err(_) => panic!("asd"),
+            Err(_) => panic!("Error writing to the TCP stream"),
         };
     }
-    //stream.write("nnldnsdknlsd".as_bytes())?;
-
-
+    
     Ok(())
-}
-
-fn listen_thread() {
-
 }
